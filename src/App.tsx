36280ca08fe153502de485fdc0f8bc75ac4b9fc2
negar feghhi <negarfeghhi@negars-MacBook-Pro.local>
2026-02-23 14:56:45 +0330

    feat(notification): add notificationitems and notifications component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { useState } from "react";
 import "./App.css";
-import EditProfileModal from "./components/EditProfileModal";
+// import EditProfileModal from "./components/EditProfileModal";
 import Navbar from "./Navbar";
+// import Notifications from "./components/Notifications";
 
 function App() {
-
-  const [open, setOpen] = useState(false)
+  // const [open, setOpen] = useState(false)
 
   return (
     <>
       <div className=" min-h-screen w-full">
         <Navbar />
-        <button onClick={()=>setOpen(true)}>Modal</button>
-        <EditProfileModal open={open} onClose={()=>setOpen(false)}/>
+        {/* <button onClick={() => setOpen(true)}>Modal</button>
+        <EditProfileModal open={open} onClose={() => setOpen(false)} />
+        <div className="flex min-h-screen items-center justify-center bg-gray-50 p-4">
+          <Notifications />
+        </div> */}
       </div>
     </>
   );
